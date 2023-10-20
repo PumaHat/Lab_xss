e6aa989f471f4e0c2c5ddf5af89252ea9f3ae30f
Giovanni Alfredo Garciliano Díaz <rapunzel@disroot.org>
2023-10-20 15:08:13 -0600

    Error now

--- xss/models.py
+++ xss/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from datetime import datetime
+from django.utils.timezone import now
 
 class Comentario(models.Model):
     texto = models.CharField(max_length=500)
     nombre = models.CharField(max_length=60)
     color = models.CharField(max_length=500)
-    fecha = models.DateTimeField(default=datetime.now())
+    fecha = models.DateTimeField(default=now)
